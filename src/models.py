06f4e5d1b71ffa2c69eea5c1dd85d684264beb72
N1kf3 <wilfredo.andrade92@gmail.com>
2023-03-09 18:20:15 +0000

    primera version

--- src/models.py
+++ src/models.py
@@ -8,26 +8,40 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    creator = Column(String(250), ForeignKey('user.user_name') ,nullable=False)
+    media = Column(String(250), ForeignKey('media.id'))
 
-    def to_dict(self):
-        return {}
+class Comment(Base):
+    __tablename__='comment'
+    id = Column(Integer, primary_key=True)
+    post = Column(String(250), ForeignKey('post.id'))
+    user = Column(String(250), ForeignKey('user.id'))
+    content = Column(String(250))
+
+class Story(Base):
+    __tablename__ = 'story'
+    id = Column(Integer, primary_key=True)
+    user = Column(String(250), ForeignKey('user.id'))
+    media = Column(String(250), ForeignKey('media.id'))
+
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    content = Column(String(250) ,nullable=False)
+
+
+
 
 ## Draw from SQLAlchemy base
 try:
